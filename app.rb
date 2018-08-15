5339a1a7ec650ab217359c4992770f61e109f403
Sarita Raghunath <sarita.raghunath@gmail.com>
2018-08-15 13:33:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
-end
+class App < Sinatra::Base 
+  get '/' name 
+    "My name is Sarita"
+  end
+
+  get '/' hometown
+    "My hometown is Princeton"
+  end 
+
+  get '/' favorite-song 
+    "My favorite song is Maps by Maroon 5"
+  end 
+
+end 
